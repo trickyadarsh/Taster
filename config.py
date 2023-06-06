5abe09833e54e7a4ed6f011798d84cec9109b106
trickyadarsh <121372273+trickyadarsh@users.noreply.github.com>
2023-06-06 13:16:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5844064152").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DarkAarush/new")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/trickyadarsh/Taster")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Cute_Copy_Aarush")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/plovestatus")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/apani_dosti")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
